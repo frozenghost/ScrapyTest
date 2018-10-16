f0628fec4c2624ad275fa941ec85d314b3d9a051
tim.huang <hc092783@gmail.com>
2018-10-16 15:22:20 +0800

    下载中间件改为通用，浏览器设置放在spider中

--- scrapytest/middlewares.py
+++ scrapytest/middlewares.py
@@ -12,100 +12,77 @@ import time
 import random
 import base64
 import re
-from settings import PROXIES
 
 
 class SeleniumMiddleware():
-    def __init__(self, timeout=None, service_args=[]):
-        chromeoption = webdriver.ChromeOptions()
-        # for option in service_args:
-        #     chromeoption.add_argument(option)
-        proxy = random.choice(PROXIES)
-        # chromeoption.add_argument('--proxy-server=' + proxy['ip_port'])
-
-        self.logger = getLogger(__name__)
-        self.timeout = timeout
-        self.browser = webdriver.Chrome()
-        self.browser.set_window_size(1400, 700)
-        self.browser.set_page_load_timeout(self.timeout)
-        self.wait = WebDriverWait(self.browser, self.timeout)
+    '''通用selenium下载中间件，通过spider的浏览器访问页面并返回response
+    
+    Returns:
+        HtmlResponse -- 返回载入后的请求
+    '''
 
     def process_request(self, request, spider):
-        """
-        用PhantomJS抓取页面
-        :param request: Request对象
-        :param spider: Spider对象
-        :return: HtmlResponse
-        """
-        for cookie_name, cookie_value in request.cookies.items():
-            self.browser.add_cookie({
-                'name': cookie_name,
-                'value': cookie_value
-            })
-        time.sleep(random.randint(1, 7))
-        page = request.meta.get('page', 1)
-        need = True
-        try:
-            self.browser.get(request.url)
-            if page > 1:
-                input = self.wait.until(
-                    EC.presence_of_element_located(
-                        (By.CSS_SELECTOR, '#resultList div.p_in > input')))
-                submit = self.wait.until(
-                    EC.element_to_be_clickable(
-                        (By.CSS_SELECTOR,
-                         '#resultList div.p_in > span.og_but')))
-                max_page = self.browser.find_element_by_xpath(
-                    '(//div[@id="resultList"]//div[@class="p_in"]//span[@class="td"])[1]'
-                ).text
-                match = re.search('[1-9]\\d*', max_page)
-                if (match):
-                    max_page = match.group()
-                need = int(max_page) >= page
-                input.clear()
-                input.send_keys(page)
-                time.sleep(random.randint(1, 7))
-                if (need):
-                    submit.click()
-                    self.wait.until(
-                        EC.text_to_be_present_in_element(
+        if (spider.name == 'fojob'):
+            for cookie_name, cookie_value in request.cookies.items():
+                spider.browser.add_cookie({
+                    'name': cookie_name,
+                    'value': cookie_value
+                })
+            time.sleep(random.randint(1, 7))  # 随机等待
+            page = request.meta.get('page', 1)
+            need = True  # 最大页数是否大于请求页数
+            try:
+                spider.browser.get(request.url)
+                if page > 1:  # 超过第一页，那么寻找导航条，跳转至对应的页码
+                    input = spider.wait.until(  # 输入框
+                        EC.presence_of_element_located(
+                            (By.CSS_SELECTOR, '#resultList div.p_in > input')))
+                    submit = spider.wait.until(  # 按钮
+                        EC.element_to_be_clickable(
                             (By.CSS_SELECTOR,
-                             '#resultList div.rt > span.dw_c_orange'),
-                            str(page)))
-                    self.wait.until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR,
-                                                        '#resultList .el')))
-            if (need):
+                             '#resultList div.p_in > span.og_but')))
+                    max_page = spider.browser.find_element_by_xpath(
+                        '(//div[@id="resultList"]//div[@class="p_in"]//span[@class="td"])[1]'
+                    ).text
+                    match = re.search('[1-9]\\d*', max_page)
+                    if (match):
+                        max_page = match.group()
+                    need = int(max_page) >= page
+                    input.clear()
+                    input.send_keys(page)
+                    time.sleep(random.randint(1, 7))
+                    if (need):  # 超过最大页数则无需等待处理
+                        submit.click()
+                        spider.wait.until(
+                            EC.text_to_be_present_in_element(
+                                (By.CSS_SELECTOR,
+                                 '#resultList div.rt > span.dw_c_orange'),
+                                str(page)))
+                        spider.wait.until(
+                            EC.presence_of_element_located(
+                                (By.CSS_SELECTOR, '#resultList .el')))
+                if (need):
+                    return HtmlResponse(
+                        url=request.url,
+                        body=spider.browser.page_source,
+                        request=request,
+                        encoding='utf-8',
+                        status=200)
+                else:  # 超出最大页数，返回空（当然还有很多其他做法，个人喜好）
+                    return HtmlResponse(
+                        url=request.url,
+                        request=request,
+                        encoding='utf-8',
+                        status=200)
+            except TimeoutException:
                 return HtmlResponse(
-                    url=request.url,
-                    body=self.browser.page_source,
-                    request=request,
-                    encoding='utf-8',
-                    status=200)
-            else:
-                return HtmlResponse(
-                    url=request.url,
-                    request=request,
-                    encoding='utf-8',
-                    status=200)
-        except TimeoutException:
-            return HtmlResponse(url=request.url, status=500, request=request)
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        middleware = cls(
-            timeout=crawler.settings.get('SELENIUM_TIMEOUT'),
-            service_args=crawler.settings.get('CHROME_SERVICE_ARGS'))
-        crawler.signals.connect(middleware.spider_closed,
-                                signals.spider_closed)
-        return middleware
-
-    def spider_closed(self):
-        self.browser.quit()
+                    url=request.url, status=500, request=request)
 
 
 class RandomUserAgent(object):
-    """Randomly rotate user agents based on a list of predefined ones"""
+    '''随机用户请求头
+
+    '''
 
     def __init__(self, agents):
         self.agents = agents
--- scrapytest/spiders/fojobspider.py
+++ scrapytest/spiders/fojobspider.py
@@ -1,9 +1,16 @@
-import scrapy
 from customer_constant import FO_Salary
-from scrapy import Request, Spider
+from scrapy import Request, Spider, signals
 from urllib.parse import quote
 from scrapy.utils.project import get_project_settings
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from items import JobItem
+from settings import PROXIES
+import random
+import scrapy
 
 
 class Fojobspider(Spider):
@@ -11,13 +18,31 @@ class Fojobspider(Spider):
     allowed_domains = ["www.51job.com"]
     base_url = "https://search.51job.com/list/020000,000000,0000,00,9,%02d,%s,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99%s&lonlat=0%%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
 
-    custom_settings = {'KEYWORDS': ['.net'], 'MAX_PAGE': 100, 'SALARY_RANGE':[FO_Salary.S_20K_30k, FO_Salary.S_30k_40k]}
+    custom_settings = {
+        'KEYWORDS': ['.net'],
+        'MAX_PAGE': 100,
+        'SALARY_RANGE': [FO_Salary.S_20K_30k, FO_Salary.S_30k_40k]
+    }
+
+    def __init__(self, timeout=None, service_args=[]):
+        chromeoption = webdriver.ChromeOptions()
+        for option in service_args:
+            chromeoption.add_argument(option)
+        # proxy = random.choice(PROXIES)
+        # chromeoption.add_argument('--proxy-server=' + proxy['ip_port'])
+        self.timeout = timeout
+        self.browser = webdriver.Chrome(chrome_options=chromeoption)
+        self.browser.set_window_size(1400, 700)
+        self.browser.set_page_load_timeout(self.timeout)
+        self.wait = WebDriverWait(self.browser, self.timeout)
 
     def start_requests(self):
-        for keyword in self.custom_settings['KEYWORDS']:
-            for salary in self.custom_settings['SALARY_RANGE']:
-                for page in range(1, 10):
-                    url = self.base_url % (salary.value, quote(keyword), '' if(salary != FO_Salary.ALL) else '&providesalary=99')
+        for keyword in self.custom_settings['KEYWORDS']:  # 职位关键字
+            for salary in self.custom_settings['SALARY_RANGE']:  # 薪资范围
+                for page in range(1, 10):  # 取前10页数据
+                    url = self.base_url % (salary.value, quote(keyword), '' if
+                                           (salary != FO_Salary.ALL) else
+                                           '&providesalary=99')
                     yield scrapy.Request(
                         url=url,
                         callback=self.parse,
@@ -30,8 +55,7 @@ class Fojobspider(Spider):
         for product in products:
             item = JobItem()
             item['name'] = ''.join(
-                product.xpath(
-                    './/p[contains(@class, "t1")]//span//a/@title').
+                product.xpath('.//p[contains(@class, "t1")]//span//a/@title').
                 extract()).strip()
             item['company'] = ''.join(
                 product.xpath('.//span[contains(@class, "t2")]//a/@title').
@@ -43,3 +67,15 @@ class Fojobspider(Spider):
             item['publishdate'] = product.xpath(
                 './/span[contains(@class, "t5")]//text()').extract_first()
             yield item
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        middleware = cls(
+            timeout=crawler.settings.get('SELENIUM_TIMEOUT'),
+            service_args=crawler.settings.get('CHROME_SERVICE_ARGS'))
+        crawler.signals.connect(middleware.spider_closed,
+                                signals.spider_closed)  # 信号量关联，spider退出之后关闭浏览器
+        return middleware
+
+    def spider_closed(self):
+        self.browser.quit()
